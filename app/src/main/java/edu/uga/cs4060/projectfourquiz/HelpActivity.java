77850cfb289492aa6ad0c749a1f56e61c50b6dd2
Alex <alexdeng201@gmail.com>
2018-10-23 19:15:35 -0400

    randomized radiobuttons

---
+++ app/src/main/java/edu/uga/cs4060/projectfourquiz/HelpActivity.java
@@ -0,0 +1,14 @@
+package edu.uga.cs4060.projectfourquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+
+}
--- app/src/main/java/edu/uga/cs4060/projectfourquiz/QuizActivity.java
+++ app/src/main/java/edu/uga/cs4060/projectfourquiz/QuizActivity.java
@@ -346,4 +346,5 @@ public class QuizActivity extends AppCompatActivity
             }
         }
 
+
 }
