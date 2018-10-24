d61e424d3ffaeb7020f3a61cd170e022fa2fcf47
Ben Rigsby <ber52305@uga.edu>
2018-10-23 21:04:07 -0400

    Made quizhistory async

--- app/src/main/java/edu/uga/cs4060/projectfourquiz/LeaderBoardsActivity.java
+++ app/src/main/java/edu/uga/cs4060/projectfourquiz/LeaderBoardsActivity.java
@@ -1,5 +1,6 @@
 package edu.uga.cs4060.projectfourquiz;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -16,23 +17,39 @@ public class LeaderBoardsActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboards);
 
-        textView = (TextView) findViewById(R.id.textView2);
+        //Calls an async task to update the quizhistory
+        new CreateQuizHistoryList().execute(quizInstanceData);
+    }
 
-        quizInstanceData = new QuizInstanceData(this);
-        quizInstanceData.open();
-        List<QuizInstance> quizzes = quizInstanceData.retrieveAllQuizInstances();
-        quizInstanceData.close();
-        String myResults = "";
-        for(int counter = 0; counter <= quizzes.size()-1;counter++){
-            String date = quizzes.get(counter).getDate();
-            String numCorrect = Integer.toString(quizzes.get(counter).getNumCorrect());
-            String numAnswered = Integer.toString(quizzes.get(counter).getNumAnswered());
-            if(!numAnswered.equalsIgnoreCase("6"))
-                continue;
-            myResults += "Quiz on " + date + ":\n    You got " + numCorrect + " of " + numAnswered + ".\n\n";
+    /**
+     * An async task to pull the quiz history from the db and display it
+     */
+    private class CreateQuizHistoryList extends AsyncTask<QuizInstanceData ,Void, String> {
+
+        @Override
+        protected String doInBackground(QuizInstanceData... quizInstanceData1){
+            textView = (TextView) findViewById(R.id.textView2);
+
+            quizInstanceData = new QuizInstanceData(getBaseContext());
+            //Open the db to read
+            quizInstanceData.open();
+            //retrieve all the past quizzes
+            List<QuizInstance> quizzes = quizInstanceData.retrieveAllQuizInstances();
+            quizInstanceData.close();
+            String myResults = "";
+            for(int counter = 0; counter <= quizzes.size()-1;counter++){
+                //Get all the db info we need
+                String date = quizzes.get(counter).getDate();
+                String numCorrect = Integer.toString(quizzes.get(counter).getNumCorrect());
+                String numAnswered = Integer.toString(quizzes.get(counter).getNumAnswered());
+                //we want to ignore the rows that are incomplete quizzes (should only ever be the last one)
+                if(!numAnswered.equalsIgnoreCase("6"))
+                    continue;
+                myResults += "Quiz on " + date + ":\n     You got " + numCorrect + " of " + numAnswered + " correct.\n\n";
+            }
+            textView.setText(myResults);
+            return myResults;
         }
-        textView.setText(myResults);
-
     }
 
 }
